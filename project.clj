cde4732a94c3c390634fb2ed7ac4517f42d2e86c
Roman Gonzalez <romanandreg@gmail.com>
2012-02-19 17:24:33 -0800

    Releasing the 0.0.2 version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.van-clj/zetta-river "0.0.2-SNAPSHOT"
+(defproject org.van-clj/zetta-river "0.0.2"
   :description "Integration between the river library and the
                 zetta-parser library"
   :author "Roman Gonzalez"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.van-clj/river "0.1.0-SNAPSHOT"]
+                 [org.van-clj/river "0.1.0"]
                  [org.van-clj/zetta-parser "0.0.2"]])
